0d6d28db6763efdea28e908a9ed3caabf92a5d7d
banziyeoh <yh_yeoh10@hotmail.com>
2016-11-02 22:24:27 +0800

    //V0.10.1.2.1100

--- app/src/main/java/nev/com/controller/MainActivity.java
+++ app/src/main/java/nev/com/controller/MainActivity.java
@@ -1,18 +1,17 @@
 package nev.com.controller;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Toast;
-import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     Button accelerate,right,left,reverse,settings;
     private BluetoothAdapter blueth;
-    ToggleButton tb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,26 +21,23 @@ public class MainActivity extends AppCompatActivity {
         right = (Button)findViewById(R.id.right);
         left = (Button)findViewById(R.id.left);
         reverse = (Button)findViewById(R.id.reverse);
-        tb = (ToggleButton)findViewById(R.id.toggleButton) ;
         settings = (Button)findViewById(R.id.settings);
-        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        settings.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b == true){
-                    ;
+            public void onClick(View view) {
+                final Intent intent = new Intent(Intent.ACTION_MAIN, null);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                ComponentName cn = new ComponentName("com.android.settings",
+                        "com.android.settings.bluetooth.BluetoothSettings");
+                intent.setComponent(cn);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity( intent);
 
-                        Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                        startActivityForResult(turnOn,0);
-                        Toast.makeText(getApplicationContext(),"Turned ON",Toast.LENGTH_SHORT).show();
-
-                    } else{
-                        Toast.makeText(getApplicationContext(),"Turned OFF",Toast.LENGTH_SHORT).show();
-
-                    }
-                }
-
-
+            }
         });
+
+
+
 
 
 
